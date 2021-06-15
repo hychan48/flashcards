a7cd6de4e5e4f8543e5e5a75a47df8556420424b
JC <jchan48@gmail.com>
2021-06-15 03:21:01 -0400

    updated deploy script back to gh-pages

--- dev/generatorFlashcards/dataFlashcards.test.mjs
+++ dev/generatorFlashcards/dataFlashcards.test.mjs
@@ -1,5 +1,5 @@
-// import { createRequire } from 'module';
-// const require = createRequire(import.meta.url);
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 // const assert = require('assert');
 // const {describe,it} = require('mocha');
 import assert from 'assert';
@@ -17,14 +17,14 @@ npm i esm module-alias mocha --save-dev
 test specific timeout
 this.timeout(500);//500ms
 */
-// const fs = require('fs');
+const fs = require('fs');
 // console.log(fs);
 
 
 /**
  * Generate dataset into static in json form
 
-
+ https://ketaminenightmares.com/pex/other/memorisation_material/graphs/cardiovascular_physiology_graphs.htm
  $$('span').map(val => val.textContent).filter(val => /\w+/g.test(val))
 
  $$('img').map(val => val.src)
@@ -39,6 +39,29 @@ describe('Datasets', function(){
         { title: 'Best airlines', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 6 },
       ]
     ]
+
+    const texts = ["Cardiovascular Physiology Graphs", "Aortic vs radial waveform",
+      "Coronary blood flow", "LV pressure-volume loop", "Wigger’s diagramme",
+      "Note the LV and aortic pressure waveforms are\n  frequently drawn incorrectly.",
+      "Pacemaker action potential", "Ventricular myocyte action\n  potential", "Pulmonary vascular\n  resistance",
+      "Venous return and cardiac\n  output curves", "Cardiac output\n  thermodilution"
+    ];
+    const imgURls = ["https://ketaminenightmares.com/pex/other/memorisat…rdiovascular_physiology_graphs_files/image001.jpg", "https://ketaminenightmares.com/pex/other/memorisat…rdiovascular_physiology_graphs_files/image002.jpg", "https://ketaminenightmares.com/pex/other/memorisat…rdiovascular_physiology_graphs_files/image003.jpg", "https://ketaminenightmares.com/pex/other/memorisat…rdiovascular_physiology_graphs_files/image004.jpg", "https://ketaminenightmares.com/pex/other/memorisat…rdiovascular_physiology_graphs_files/image005.jpg", "https://ketaminenightmares.com/pex/other/memorisat…rdiovascular_physiology_graphs_files/image006.jpg", "https://ketaminenightmares.com/pex/other/memorisat…rdiovascular_physiology_graphs_files/image007.jpg", "https://ketaminenightmares.com/pex/other/memorisat…rdiovascular_physiology_graphs_files/image008.jpg", "https://ketaminenightmares.com/pex/other/memorisat…rdiovascular_physiology_graphs_files/image009.jpg", "https://ketaminenightmares.com/pex/other/memorisat…rdiovascular_physiology_graphs_files/image010.jpg", "https://ketaminenightmares.com/pex/other/memorisat…rdiovascular_physiology_graphs_files/image011.jpg"]
+
+    const aOut = [];
+    for (let i = 0; i < texts.length; i++) {
+      const text = texts[i];
+      const imgURL = imgURls[i];
+
+      const tmp = {
+
+      };
+      aOut.push(tmp);
+    }
+
+    const jsonOut = JSON.stringify(aOut,null,2);
+    // const jsonOut = JSON.stringify(aOut,null,2);
+    fs.writeFileSync("static/flashcards/dv1.json",jsonOut);
 
     assert.strictEqual(1,2)
 
