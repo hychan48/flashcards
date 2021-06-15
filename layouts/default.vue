df1b3a79592aed64f982e14539d97128e13ba7cb
Jason <jchan48@gmail.com>
2021-06-14 22:02:34 -0400

    updating text

--- layouts/default.vue
+++ layouts/default.vue
@@ -103,8 +103,8 @@ export default {
         },
         {
           icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
+          title: 'Legal',
+          to: '/legal'
         }
       ],
       miniVariant: false,
