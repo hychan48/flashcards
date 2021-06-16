ddf207e79f1a81d3e32fc7775107023599bf6d0a
jasonchan <jchan48@gmail.com>
2021-06-16 14:28:28 -0400

    got the fetch to work now. need to set it to computed / $options

--- pages/index.vue
+++ pages/index.vue
@@ -133,11 +133,11 @@ export default {
   },
   //testing better way
   async fetch() {
-    // const oData = await fetch('/flashcards/gen/tableItems.json')
-    const oData = await fetch('http://localhost:3000/flashcards/flashcards/gen/tableItems.json')
+    const oData = await fetch('/flashcards/flashcards/gen/tableItems.json');// todo append the path / baseurl
+    // const oData = await fetch('http://localhost:3000/flashcards/flashcards/gen/tableItems.json')
     // const oData = await this.$axios('/static/flashcards/gen/tableItems.json')
     // debugger
-   // alert(JSON.stringify(await oData.json()))
+   alert(JSON.stringify(await oData.json()))
   },
   computed: {
     // ...mapGetters(["pageTitle"]),
