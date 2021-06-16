b0d3a9e49986f57f1e7d3d958469603feee82cad
JC <jchan48@gmail.com>
2021-06-16 15:02:45 -0400

    nuxt import for json isnt great

--- assets/nuxtFetchStaticMixin.js
+++ assets/nuxtFetchStaticMixin.js
@@ -1,3 +1,9 @@
 /**
  * First Attempt
+ *
+ * 1. nuxt fetch is called everytime. i can use keep-alive.
+ *
+ * 2. Using the store after the fetch would cache the results as well
  */
+
+const nuxtFetchStaticMixin = {}
