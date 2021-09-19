164b3c0c5b18476f9032076a5a59dcaaac64b6ea
jasonchan <jchan48@gmail.com>
2021-09-19 13:40:37 -0400

    generated data for multiple choice 60 and the page

---
+++ pages/multichoice60.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row>
+    <v-col notes="Multi Choice 60">
+      <v-card
+        hover
+      >
+
+        <v-card-title notes="Multi Choice 60"
+
+        >
+          <v-icon
+            left
+          >
+            mdi-bell-ring
+          </v-icon>
+          <span class="title font-weight-light">
+        Multi Choice 60
+      </span>
+        </v-card-title>
+        <v-card-text>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+
+</template>
+
+<script>
+export default {
+  name: "multichoice60",
+
+}
+</script>
+
+<style scoped>
+
+</style>
